192494e36e97cc9f081fa33f9354404aec3d844b
Brennan Fife <brenfife@gmail.com>
2022-12-01 10:39:25 -0700

    -

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { WagmiConfig, createClient, chain } from "wagmi";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
+
+const client = createClient(
+  getDefaultClient({
+    appName: "ConnectKit Next.js demo",
+    alchemyId: "FZ-chszarh12L1wVI9gpoX77YholcKgc",
+    chains: [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  })
+);
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <WagmiConfig client={client}>
+      <ConnectKitProvider
+      // customTheme={{
+      //   // "--ck-connectbutton-background": "#ff0",
+      //   "--ck-connectbutton-border-radius": 40,
+      // }}
+      >
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 }
+
+export default MyApp;
